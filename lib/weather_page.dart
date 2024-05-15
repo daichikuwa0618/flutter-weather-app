aaf2bd174f84bd616457e2d632dab0b2d869ec3a
Daichi Hayashi <develop@daichidaiji.com>
2024-05-15 17:03:39 +0900

    [Issue-#2] 天気予報を表示する UI を実装
    fix 不要なパラメータの削除
    
    Column で SizedBox と _ButtonsRow を配置する形から Align + Padding という形に修正

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -7,8 +7,106 @@ class WeatherPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Hello World!'),
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Column(
+            children: [
+              // NOTE: [_ForecastContent] を上下中央に表示するために
+              //       [flex: 1] を設定してある [Spacer] および [Expanded] で挟んでいる。
+              //       [Column] に要素を追加すると上下中央のレイアウトが崩れるため注意。
+              Spacer(),
+              _ForecastContent(),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 80),
+                    child: _ButtonsRow(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class _ForecastContent extends StatelessWidget {
+  const _ForecastContent();
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      children: [
+        const AspectRatio(aspectRatio: 1, child: Placeholder(),),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '** ℃',
+                  textAlign: TextAlign.center,
+                  style: textTheme.labelLarge?.copyWith(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  '** ℃',
+                  textAlign: TextAlign.center,
+                  style: textTheme.labelLarge?.copyWith(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ButtonsRow extends StatelessWidget {
+  const _ButtonsRow();
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Close',
+              textAlign: TextAlign.center,
+              style: textTheme.labelLarge?.copyWith(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Reload',
+              textAlign: TextAlign.center,
+              style: textTheme.labelLarge?.copyWith(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
