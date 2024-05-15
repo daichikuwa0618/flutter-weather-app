ae6ed6403aac5d4123780a85d7b3482a8b32f49c
Daichi Hayashi <develop@daichidaiji.com>
2024-05-15 14:29:18 +0900

    MainApp -> App にリネームし Widget tree の Scaffold 以下を weather_page に切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/weather_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Weather App',
+      home: WeatherPage(),
     );
   }
 }
