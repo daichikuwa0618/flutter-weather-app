da8c79261fa078e95de283ed4f2df71835648fb0
Daichi Hayashi <develop@daichidaiji.com>
2024-05-16 09:54:02 +0900

    [Issue-#3] WeatherCondition から生成される天気画像の Widget を追加

---
+++ lib/weather_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_training/data/weather_condition.dart';
+
+class WeatherIcon extends StatelessWidget {
+  const WeatherIcon({super.key, WeatherCondition? weatherCondition})
+    : _weatherCondition = weatherCondition;
+
+  final WeatherCondition? _weatherCondition;
+
+  @override
+  Widget build(BuildContext context) {
+    final content = _weatherCondition != null
+      ? SvgPicture.asset('assets/${_weatherCondition.name}.svg')
+      : const Placeholder();
+    return AspectRatio(
+      aspectRatio: 1,
+      child: content,
+    );
+  }
+}
