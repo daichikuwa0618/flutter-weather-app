17fad8e9932ea6a4b2636032bebc1f2e68611d87
Daichi Hayashi <develop@daichidaiji.com>
2024-05-17 15:46:10 +0900

    [Issue-#4] LaunchScreen -> WeatherScreen への遷移を実装

--- lib/launch/launch_screen.dart
+++ lib/launch/launch_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LaunchScreen extends StatefulWidget {
   const LaunchScreen({super.key});
@@ -8,6 +9,27 @@ class LaunchScreen extends StatefulWidget {
 }
 
 class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(_showWeatherAfterRendering);
+  }
+
+  Future<void> _showWeatherAfterRendering() async {
+    await WidgetsBinding.instance.endOfFrame;
+    await _showWeather();
+  }
+
+  Future<void> _showWeather() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+    if (mounted) {
+      await context.push('/weather');
+      // WeatherScreen にて "Close" が押された場合や iOS のエッジスワイプで戻った際に
+      // 再度 WeatherScreen を表示するために再帰呼び出し
+      await _showWeather();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_training/launch/launch_screen.dart';
+import 'package:flutter_training/weather/use_case/get_weather.dart';
+import 'package:flutter_training/weather/weather_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
@@ -6,6 +8,10 @@ final router = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const LaunchScreen(),
+    ),
+    GoRoute(
+      path: '/weather',
+      builder: (context, state) => const WeatherScreen(GetWeather()),
     ),
   ],
 );
