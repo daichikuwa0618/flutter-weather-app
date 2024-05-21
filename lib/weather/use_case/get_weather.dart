5bec5555edf43f6203b1d1175aa4f6f2a082d4e7
Daichi Hayashi <develop@daichidaiji.com>
2024-05-21 13:01:58 +0900

    [Issue-#6] 発生しうる例外をアプリケーション内に定義し UseCase 内でハンドリング

--- lib/weather/use_case/get_weather.dart
+++ lib/weather/use_case/get_weather.dart
@@ -1,11 +1,48 @@
 import 'package:flutter_training/data/weather_condition.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
+sealed class GetWeatherException implements Exception {
+  const GetWeatherException({YumemiWeatherError? rawError})
+      : _rawError = rawError;
+
+  final YumemiWeatherError? _rawError;
+
+  @override
+  String toString() => _rawError.toString();
+}
+
+final class UnknownException extends GetWeatherException {
+  const UnknownException({super.rawError});
+
+  @override
+  String toString() => 'Unknown Exception: $_rawError';
+}
+
+final class InvalidParameterException extends GetWeatherException {
+  const InvalidParameterException({super.rawError});
+
+  @override
+  String toString() => 'Parameter is not valid: $_rawError';
+}
+
 final class GetWeather {
   const GetWeather();
 
   WeatherCondition call({required String area}) {
-    final response = YumemiWeather().fetchThrowsWeather(area);
-    return WeatherCondition.values.byName(response);
+    try {
+      final response = YumemiWeather().fetchThrowsWeather(area);
+      return WeatherCondition.values.byName(response);
+    } on YumemiWeatherError catch(e) {
+      switch (e) {
+        case YumemiWeatherError.unknown:
+          throw UnknownException(rawError: e);
+
+        case YumemiWeatherError.invalidParameter:
+          throw InvalidParameterException(rawError: e);
+      }
+    } on Exception catch(_) {
+      assert(false, 'Unexpected Exception');
+      throw const UnknownException();
+    }
   }
 }
